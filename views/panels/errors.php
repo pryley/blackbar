6623191f60d42d95367ad1a3225354a80bfe4131
Paul Ryley <paul@geminilabs.io>
2018-02-22 19:33:52 -0700

    add “No errors found.”

--- views/panels/errors.php
+++ views/panels/errors.php
@@ -2,11 +2,17 @@
 
 <table>
 	<tbody>
+		<?php if( empty( $errors )) : ?>
+		<tr>
+			<td><?= __( 'No errors found.', 'blackbar' ); ?></td>
+		</tr>
+		<?php else : ?>
 		<?php foreach( $errors as $error ) : ?>
 		<tr>
 			<td><?= $error['name']; ?></td>
 			<td><?= $error['message']; ?></td>
 		</tr>
 		<?php endforeach; ?>
+		<?php endif; ?>
 	</tbody>
 </table>
