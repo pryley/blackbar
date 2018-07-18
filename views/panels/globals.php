ebd12d73961f6c82c7e737a01f4039c2b6fe33e3
Paul Ryley <paul@geminilabs.io>
2018-07-18 14:26:50 -0600

    fix $_SESSION

--- views/panels/globals.php
+++ views/panels/globals.php
@@ -3,5 +3,5 @@
 <pre><code class="php">$_GET = <?= esc_html( var_export( $_GET, true )); ?>;</code></pre><br/>
 <pre><code class="php">$_POST = <?= esc_html( var_export( $_POST, true )); ?>;</code></pre><br/>
 <pre><code class="php">$_COOKIE = <?= esc_html( var_export( $_COOKIE, true )); ?>;</code></pre><br/>
-<pre><code class="php">$_SESSION = <?= esc_html( var_export( $_SESSION, true )); ?>;</code></pre><br/>
+<pre><code class="php">$_SESSION = <?= esc_html( var_export( isset( $_SESSION ) ? $_SESSION : [], true )); ?>;</code></pre><br/>
 <pre><code class="php">$_SERVER = <?= esc_html( var_export( $_SERVER, true )); ?>;</code></pre><br/>
