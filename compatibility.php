3e05db5f378cb33f82b839cc1dc5a44a72704252
Paul Ryley <paul@geminilabs.io>
2023-09-18 12:57:06 +0100

    fix php7 support

--- compatibility.php
+++ compatibility.php
@@ -2,6 +2,12 @@
 
 defined('WPINC') || exit;
 
+if (!function_exists('str_starts_with')) {
+    function str_starts_with(?string $haystack, ?string $needle): bool {
+        return 0 === strncmp((string) $haystack, (string) $needle, strlen((string) $needle));
+    }
+}
+
 /*
  * @return array
  * @see https://docs.gravityforms.com/gform_noconflict_scripts/
